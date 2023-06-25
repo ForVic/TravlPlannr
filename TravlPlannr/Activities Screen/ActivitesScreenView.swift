0dbddb5bbe8f409c8919d19c2e7da8d3bc6914d9
Dustin Noonan <dustin1.noonan@gmail.com>
2023-06-24 20:32:30 -0400

    Do not approve of pr. I pulled changes and it broke my local clone of repo. Added functionality for Travl Destinations Screen now shows Current Location and Can search for locations via Add button. Added stubs for Activities Screen and Firebase stuff we still need to implement.

---
+++ TravlPlannr/Activities Screen/ActivitesScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivitesScreenView.swift
+//  TravlPlannrApp
+//
+//  Created by Patrick Noonan on 6/24/23.
+//
+
+import UIKit
+
+class ActivitesScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
